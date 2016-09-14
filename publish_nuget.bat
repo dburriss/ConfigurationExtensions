408e187865e4db32632ebaf6bf1475911b78d939
dburriss <devon@chimplab.co>
2016-09-14 09:36:39 +0200

    appveyor CI pipeline

--- publish_nuget.bat
+++
@@ -1,6 +0,0 @@
-dotnet restore src\ChimpLab.Extensions.Configuration.Json\project.json
-dotnet pack src\ChimpLab.Extensions.Configuration.Json\project.json -c Release -o artifacts\bin\ChimpLab.Extensions.Configuration.Json\Release
-
-set /p version="Version: "
-set /p key="Api Key: "
-nuget push artifacts\bin\ChimpLab.Extensions.Configuration.Json\Release\ChimpLab.Extensions.Configuration.Json.%version%.nupkg -Source https://www.nuget.org/api/v2/package -apikey %key%
